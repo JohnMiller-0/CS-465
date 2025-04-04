2efc3a79aa8a94cd8bf52ab56a1fa196eb6b77b2
John Miller <john.miller27@snhu.edu>
2025-04-03 20:04:53 -0400

    Module 5 Completed Baseline

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var apiRouter = require('./app_api/routes/index');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
@@ -17,6 +18,8 @@ var handlebars = require('hbs');
 handlebars.registerHelper('eq', function(a, b) {
   return a === b;
 });
+
+require('./app_api/models/db');
 
 var app = express();
 
@@ -38,6 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/api', apiRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
